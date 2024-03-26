11badbc7d8128559cb5f8bf78f71291c9aa17cbc
nawabreihan <nawabreihan453@gmail.com>
2024-03-26 23:45:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client' ;
+import MovieCard from './MovieCard';
+
+
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+
